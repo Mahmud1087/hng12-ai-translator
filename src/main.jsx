2f4bac096a78e7571b619a66fda197790a6a62d0
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2025-02-21 20:16:13 +0100

    add tokens dynamically

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,21 @@ import AppProvider from "./context/app-provider.jsx";
 import { ToastContainer } from "react-toastify";
 import RenderApp from "./components/render-app.jsx";
 
+const translatorMeta = document.createElement("meta");
+translatorMeta.httpEquiv = "origin-trial";
+translatorMeta.content = import.meta.env.VITE_TRANSLATOR_TOKEN;
+document.head.append(translatorMeta);
+
+const detectorMeta = document.createElement("meta");
+detectorMeta.httpEquiv = "origin-trial";
+detectorMeta.content = import.meta.env.VITE_DETECTOR_TOKEN;
+document.head.append(detectorMeta);
+
+const summarizerMeta = document.createElement("meta");
+summarizerMeta.httpEquiv = "origin-trial";
+summarizerMeta.content = import.meta.env.VITE_SUMMARIZER_TOKEN;
+document.head.append(summarizerMeta);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
